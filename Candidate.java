3917c740bd3906ba8394b702661fbc3c2c84bdd3
hisidharth <hisidharth2014@gmail.com>
2024-01-30 10:06:29 -0800

    Add files via upload

---
+++ Candidate.java
@@ -0,0 +1,19 @@
+public class Candidate {
+   
+    private final int candidateID;
+    private String name;
+    private String party;
+    
+    public Candidate(int id, String name, String party){
+        candidateID = id;
+        this.name = name;
+        this.party = party;
+        
+    }
+     
+    public static void main(String[] args) {
+        
+    }
+ }
+ 
+ 
---
+++ Voter.java
@@ -0,0 +1,18 @@
+public class Voter {
+    
+    private final int voterID;
+    private String name;
+    private final boolean hasVoted;
+    
+    public Voter(int num, String name, boolean lol){
+        
+        voterID = num;
+        this.name = name;
+        hasVoted = lol;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+}
